d640b4dd536ba757b33d4ef751ed0a64067b660d
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-09-09 19:59:34 -0400

    files

---
+++ linked_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+}
+
+int main()
+{
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+    
+
+    head->data = 1;
+    head->next = second;
+}
+
+
+
