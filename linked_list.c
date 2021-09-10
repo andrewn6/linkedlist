792fa8bc7dc6990a785468b2d9fc392fa9349187
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-09-09 20:03:16 -0400

    finish

--- linked_list.c
+++ linked_list.c
@@ -19,7 +19,24 @@ int main()
 
     head->data = 1;
     head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    printList(head);
+
+    return 0;
 }
 
 
+void printList(struct Node* n)
+{
+    while (n != NULL) {
+        printf("%d ", n->data);
+        n = n->next;
+    }
+}
 
